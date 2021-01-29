fc2acc97a25eecf02407e0c48ace10eb3c01b9d0
Ariel-Reyes <73078760+Ariel-Reyes@users.noreply.github.com>
2021-01-29 13:02:30 -0600

    Menu

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	int opc=1;
+	
+	while(opc!=4){
+		cout<<"............MENU............."<<endl; 
+		cout<<"1................. Ejercicio 1 Tabla"<<endl; 
+		cout<<"2.................Ejercicio 2 Ordenar Recursivo"<<endl; 
+		cout<<"3.................Ejercicio 3 Sumatoria Recursivo"<<endl; 
+		cout<<"4................. salir"<<endl; 
+		cout<<"Ingrese la opcion que desea: "<<endl; 
+		cin>>opc; 
+		
+		switch(opc){
+			case 1:
+				break; 
+				case 2:
+					break;
+					case 3:
+						break; 
+						
+							
+		}
+		
+	}
+	 
+	
+	
+	return 0;
+}
